e9054c7c887d6995ccf4973df865c0796e45473a
동호 <65016499+dongho9@users.noreply.github.com>
2024-05-31 17:09:12 +0900

    0531 업데이트

--- script/script.js
+++ script/script.js
@@ -26,6 +26,27 @@ $(document).ready(function(){
         $('section').toggleClass('blur');
     })
 })
+
+$(document).ready(function() {
+    $('.gnb li a').click(function(e) {
+        e.preventDefault();
+        $('.gnbtoggle').removeClass('close');
+        $('html, body').removeClass('no-scroll');
+        $('.gnbwrap').removeClass('gnbtop');
+        $('section').removeClass('blur');
+        // 클릭된 메뉴 항목의 인덱스를 가져옵니다.
+        var index = $('.gnb li a').index(this);
+        console.log(index);
+
+        // 해당 인덱스의 섹션을 찾습니다.
+        var target = $('main section').eq(index);
+        
+        // 해당 섹션으로 부드럽게 스크롤합니다.
+        $('html, body').animate({
+            scrollTop: target.offset().top
+        }, 600);
+    });
+});
 $(document).ready(function(){
     $('.animate').scrolla({
         mobile:true, //모바일 버전시 활성화
